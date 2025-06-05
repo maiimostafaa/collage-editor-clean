0632352b549dc1c7c85e1901f2a230a87baf53b8
maiimostafaa <mostafa.mai876@gmail.com>
2025-06-04 19:06:16 -0700

    🚑 Force commit of new docs deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import CollageEditor from "./CollageEditor.jsx";
 
+window.alert("✅ React app mounted!");
+console.log("✅ top of main.jsx loaded");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CollageEditor />
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/collage-editor-clean/", // ✅ your GitHub repo name
   plugins: [react()],
-  base: "/collage-editor-clean/", 
+  build: {
+    minify: "esbuild", // ✅ ensures logs are not stripped
+  },
 });
